fa218697b2d4490725197231ef695eb123079852
muhammed-udaif <muhammed-udaif>
2025-11-17 09:56:11 +0530

    feat add compleate question seven

--- Day-test-second/7/index.ts
+++ Day-test-second/7/index.ts
@@ -15,6 +15,21 @@
 // integer: 1994
 
 function romanToInteger(roman: string) {
-    const romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    
+    const romans: Record<string, number> = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
+    let total = 0;
+
+    for(let i=0; i< roman.length; i++) {
+        let current = romans[roman[i]];
+        let next = romans[roman[i + 1]];
+
+        if(next && current < next) {
+            total += next - current;
+            i++;
+        } else {
+            total += current;
+        }
+    }
+    return total;
 }
+
+console.log(romanToInteger("MCMXCIV"));
