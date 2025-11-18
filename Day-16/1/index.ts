72c7c2a4d15779d499101cbe7ba715e1b2412216
muhammed-udaif <muhammed-udaif>
2025-11-18 18:45:06 +0530

    feat: day 16

---
+++ Day-16/1/index.ts
@@ -0,0 +1,32 @@
+// 1. Write a program which accepts a date and a formatter string. 
+// The program should return the given date after formatting based on the given formatter.
+
+// Supported formatters:
+
+// YY	 18	    Two-digit year
+// YYYY 2018	Four-digit year
+// M	 1-12	The month, beginning at 1
+// MM	 01-12	The month, 2-digits
+// D	 1-31	The day of the month
+// DD	 01-31	The day of the month, 2-digits
+// H	 0-23	The hour
+// HH	 00-23	The hour, 2-digits
+// h	 1-12	The hour, 12-hour clock
+// hh	 01-12	The hour, 12-hour clock, 2-digits
+// m	 0-59	The minute
+// mm	 00-59	The minute, 2-digits
+// s	 0-59	The second
+// ss	 00-59	The second, 2-digits
+// A	 AM PM	
+// a	 am pm
+
+// Test cases:
+// Input: new Date(2025, 1, 4, 13, 44, 3), 'YY-M-D, H:m:s'
+// Output: 25-2-4, 13:44:3
+
+function DateFormate(a: number, b: number, c: number, d:number, e: number, f:number) {
+    let year;
+    console.log(year)
+}
+
+console.log(DateFormate(2025, 1, 4, 13, 44, 3));
