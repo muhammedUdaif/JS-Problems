ebb01c8519a09e9150cb497bbd0ae0eb850632f2
muhammed-udaif <muhammed-udaif>
2025-11-24 17:52:45 +0530

    fix: ui

--- Day-19/2/index.js
+++ Day-19/2/index.js
@@ -1,17 +1,17 @@
-        const tabs = document.querySelectorAll('.tab');
-        const pages = document.querySelectorAll('.page');
+const tabs = document.querySelectorAll('.tab');
+const pages = document.querySelectorAll('.page');
 
-        tabs.forEach(tab => {
+tabs.forEach(tab => {
 
-            tab.addEventListener('click', () => {
-                const target = tab.getAttribute('data-target');
-                console.log(target)
+    tab.addEventListener('click', () => {
+        const target = tab.getAttribute('data-target');
+        console.log(target)
 
-                tabs.forEach(t => t.classList.remove('active'));
-                tab.classList.add('active');
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
 
-                pages.forEach(page => {
-                    page.classList.toggle('active', page.id === target);
-                });
-            });
+        pages.forEach(page => {
+            page.classList.toggle('active', page.id === target);
         });
+    });
+});
